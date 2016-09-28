7578d782c4f8bd1beb6e87c9d1c4ab553eada245
Chetan <chetanravada@gmail.com>
2016-09-28 09:18:49 +0530

    Added Page Handle code in the repo

---
+++ src/SysPage.h
@@ -0,0 +1,1 @@
+#define SYSPAGE_PAGEUNPINNED (START_SYSPAGE_WARN + 6)     // Page already unpinned
---
+++ src/SysPage_PageHandle.cc
@@ -0,0 +1,61 @@
+#include "SysPage_Internal.h"
+#include "SysPage_PageHandle.h"
+
+#define INVALID_PAGE   (-1)
+
+SysPage_PageHandle::SysPage_PageHandle()
+{
+  pageNumber = INVALID_PAGE
+  pageData = NULL
+}
+
+SysPage_PageHandle::~SysPage_PageHandle()
+{
+
+}
+
+SysPage_PageHandle::SysPage_PageHandle(const SysPage_PageHandle &pageHandle)
+{
+  // Just copy the local variables since there is no local memory
+  // allocation involved
+
+  this->pageNumber = pageHandle.pageNumber
+  this->pageData = pageHandle.pageData
+}
+
+SysPage_PageHandle& SysPage_PageHandle::operator = (const SysPage_PageHandle &pageHandle)
+{
+  // Check for self-assignment
+  if (this != &pageHandle) {
+
+    // Just copy the pointers since there is no local memory
+    // allocation involved
+    this->pageNumber = pageHandle.pageNumber;
+    this->pageData = pageHandle.pageData;
+  }
+
+  // Return a reference to this
+  return (*this);
+}
+
+ErrCode SysPage_PageHandle::getData(char *&pData)
+{
+  // Page must refer to a pinned page
+  if(pageData == NULL)
+    return SYSPAGE_PAGEUNPINNED;
+
+  pData = pageData;
+
+  return 0;
+}
+
+ErrCode SysPage_PageHandle::getPageNum(int &pageNum)
+{
+  // Page must refer to a pinned page
+  if(pageData == NULL)
+    return SYSPAGE_PAGEUNPINNED;
+
+  pageNum = pageNumber
+
+  return 0;
+}
