21dfe14d0bfac5953281e10debdfa528cc3a7cd1
Chetan <chetanravada@gmail.com>
2016-09-28 09:42:38 +0530

    defined error control variables

--- src/xbase.h
+++ src/xbase.h
@@ -18,7 +18,14 @@ enum AttrType {
    STRING
 };
 
-#define START_SYSPAGE_WARN 1
+typedef int ErrCode;
+
+#define START_SYSPAGE_ERR  (-1)
+#define END_SYSPAGE_ERR    (-100)
+
+#define START_SYSPAGE_WARN  1
+#define END_SYSPAGE_WARN    100
+
 
 //
 // TRUE, FALSE and BOOLEAN
