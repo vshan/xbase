5e1384da3231d9f41166fa52940c6df33949a6b6
vinaymundada27 <vinaymundada27@gmail.com>
2016-09-29 23:10:02 +0530

    Function prototypes of the file

---
+++ vinaym/src/SysPage_Manager.cc
@@ -0,0 +1,37 @@
+#include<cstdio>
+#include<fcntl>
+
+#include "SYSPAGE_internal.h"
+#include "SysPage_FileHandle.h"
+
+// Constructor
+SysPage_Manager::SysPage_Manager()
+{
+
+}
+
+// Destructor
+SysPage_Manager::~SysPage_Manager()
+{
+
+}
+
+ErrCode SysPage_Manager::createFile(char* fileName)
+{
+
+}
+
+ErrCode SysPage_Manager::openFile(char* fileName)
+{
+
+}
+
+ErrCode SysPage_Manager::closeFile(char* fileName)
+{
+
+}
+
+ErrCode SysPage_Manager::destroyFile(char* fileName)
+{
+
+}
