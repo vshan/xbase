62e44b25a8050c6a304c1a99122034ced9001243
Vinay Bhat <vinaybhat001@gmail.com>
2016-10-19 10:42:32 +0530

    b+tree

--- src/btree_node.cc
+++ src/btree_node.cc
@@ -143,3 +143,228 @@ void* BTreeNode::largestKey()
 		return key;
 	}
 }
+
+ErrCode BTreeNode::getKey(int pos, void* &key)
+{
+	if (pos >= 0 && pos < numKeys)
+	{
+		key = keys + attrLength*pos;
+		return 0;
+	}
+	else
+		return -1;
+}
+
+int BTreeNode::copyKey(int pos, void* toKey)
+{
+	if (toKey == NULL)
+		return -1;
+	if (pos >= 0 && pos < order) {
+		memcpy(toKey,
+			   keys + attrLength*pos,
+			   attrLength);
+		return 0;
+	}
+	else
+		return -1;
+}
+
+int BTreeNode::setKey(int pos, const void* newkey)
+{
+	if (newkey == (keys + attrLength * pos)) // should never happen
+		return 0;
+	if (pos >= 0 && pos < order)
+	{
+		memcpy(keys + attrLength*pos,
+			newkey,
+			attrLength);
+		return 0;
+	}
+	else
+		return -1;
+}
+
+int BTreeNode::insert(const void* newkey, const RID& rid)
+{
+	if (numKeys >= order)
+		return -1;
+	int i = -1;
+	void *prevKey = NULL;
+	void *currKey = NULL;
+	for (i = numKeys-1; i >= 0; i--)
+	{
+		prevKey = currKey;
+		getKey(i, currKey);
+		if (cmpKey(newkey, currKey) >= 0)
+			break;
+		rids[i+1] = rids[i];
+		setKey(i+1, currKey);
+	}
+
+	rids[i+1] = rid;
+	setKey(i+1, newkey);
+	setNumKeys(getNumKeys()+1);
+	return 0;
+}
+
+int BTreeNode::remove(const void* newkey, int kpos)
+{
+	int pos = -1;
+	if (kpos != -1) {
+		if (kpos < 0 || kpos >= numKeys)
+			return -2;
+	    pos = kpos;
+	}
+	else {
+		pos = findKey(newkey);
+		if (pos < 0)
+			return -2;
+	}
+	for (int i = pos; i < numKeys-1;i++)
+	{
+		void *p;
+		getKey(i+1, p);
+		setKey(i, p);
+		rids[i] = rids[i+1];
+	}
+	setNumKeys(getNumKeys()-1);
+	if (numKeys == 0)
+		return -1;
+	return 0;
+}
+
+RID BTreeNode::findAddrAtPosition(const void* &key)
+{
+	int pos = findKeyPosition(key);
+	if (pos == -1 || pos >= numKeys)
+		return RID(-1,-1);
+	return rids[pos];
+}
+
+int BTreeNode::findKeyPosition(const void* &key)
+{
+	for (int i = 0; i < numKeys-1; i >= 0; i--)
+	{
+		void *k;
+		if (getKey(i,k) != 0)
+			return -1;
+		if (cmpKey(key,k) == 0)
+			return i;
+		if (cmpKey(key,k) > 0)
+			return i+1;
+	}
+	return 0;
+}
+
+RID BTreeNode::getAddr(const int pos)
+{
+	if (pos < 0 || pos > numKeys)
+		return RID(-1,-1);
+	return rids[pos];
+}
+
+RID BTreeNode::findAddr(const void* &key)
+{
+	int pos = findKey(key);
+	if (pos == -1)
+		return RID(-1,-1);
+	return rids[pos];
+}
+
+RID BTreeNode::findKey(const void* &key, const RID& r)
+{
+	for (int i = numKeys-1, i >= 0; i--)
+	{
+		void *k;
+		if (getKey(i, k) != 0)
+			return -1;
+		if (cmpKey(key, k) == 0) {
+			if (r == RID(-1,-1))
+				return i;
+			else {
+				if (rids[i] == r)
+					return i;
+			}
+		}
+	}
+	return -1;
+}
+
+int BTreeNode::cmpKey(const void * a, const void * b)
+{
+	if (attrType == STRING) {
+		return memcmp(a, b, attrLength);
+	}
+
+	if (attrType == FLOAT) {
+		typedef float MyType;
+		if (*(MyType*)a > *(MyType*)b) return 1;
+		if (*(MyType*)a == *(MyType*)b) return 1;
+		if (*(MyType*)a < *(MyType*)b) return -1;
+	}
+
+	if (attrType == INT) {
+		typedef int MyType;
+		if (*(MyType*)a > *(MyType*)b) return 1;
+		if (*(MyType*)a == *(MyType*)b) return 1;
+		if (*(MyType*)a < *(MyType*)b) return -1;
+	}
+    
+    return 0;
+}
+
+ErrCode BTreeNode::split(BTreeNode* rhs)
+{
+	int firstMovedPos = (numKeys+1)/2;
+	int moveCount = (numKeys - firstMovedPos);
+
+	if ( (rhs->getNumKeys() + moveCount)
+		 > rhs->getMaxKeys())
+		return -1;
+
+	for (int pos = firstMovedPos; pos < numKeys; pos++) {
+		RID r = rids[pos];
+		void * k = NULL; this->getKey(pos, k);
+		ErrCode ec = rhs->insert(k, r);
+		if (ec != 0)
+			return ec;
+	}
+
+	for (int i = 0; i < moveCount; i++) {
+		ErrCode ec = this->remove(NULL, firstMovedPos);
+		if (ec < -1)
+			return ec;
+	}
+
+	rhs->setRight(this->getRight());
+	this->setRight(rhs->getPageRID().Page());
+	rhs->setLeft(this->getPageRID().Page());
+
+	return 0;
+}
+
+ErrCode BTreeNode::merge(BTreeNode* other)
+{
+	if (numKeys + other->getNumKeys() > order)
+		return -1;
+
+	for (int pos = 0; pos < other->getNumKeys(); pos++) {
+		void * k = NULL; other->getKey(pos, k);
+		RID r = other->getAddr(pos);
+		ErrCode ec = this->insert(k, r);
+		if (ec != 0) return ec;
+	}
+
+	int moveCount = other->getNumKeys();
+	for (int i = 0; i < moveCount; i++) {
+		ErrCode ec = other->remove(NULL, 0);
+		if (ec < -1) return ec;
+	}
+
+	if (this->getPageRID().Page() == other->getLeft())
+		this->setRight(other->getRight());
+	else
+		this->setLeft(other->getLeft());
+
+	return 0;
+}
