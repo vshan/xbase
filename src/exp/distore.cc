616fe82dcc49273a9a8ad00e34021f682c12eae5
Vinay Bhat <vinaybhat001@gmail.com>
2016-10-26 01:39:19 +0530

    fleshed out diststore

--- src/exp/distore.cc
+++ src/exp/distore.cc
@@ -2,7 +2,44 @@
 #include <ofstream>
 #include <ifstream>
 #include <boost/filesystem.hpp>
-#include "distore.h"
+#include <boost/asio.hpp>
+#include <string>
+
+typedef int StatusCode;
+
+struct DS_FileHeader {
+  int firstFree;
+  int numPages;
+};
+
+class DS_Manager {
+public:
+  DS_Manager();
+  ~DS_Manager();
+  StatusCode createFile(const char *filename);
+  StatusCode loadFile(const char *filename, DS_FileH &fileHandle);
+private:
+  DS_BufferManager *bm;
+  DS_RemoteManager *rm;
+};
+
+class DS_FileH {
+public:
+  DS_FileH();
+  ~DS_FileH();
+
+  StatusCode getPage(int pageNum, DS_PageH &pageHandle);
+  StatusCode allocatePage(DS_PageH &pageHandle);
+  StatusCode markDirty(int pageNum);
+  StatusCode unpinPage(int pageNum);
+private:
+  DS_BufferManager *bm;
+  DS_RemoteManager *rm;
+  DS_FileHeader hdr;
+  bool isRemote;
+  string ipaddr;
+  string port;
+};
 
 DS_Manager::DS_Manager()
 {
@@ -60,16 +97,93 @@ StatusCode DS_Manager::loadFile(const char *fileName,
     fileHandle.rm = rm;
     fileHandle.bm = bm;
     sc = DS_SUCCESS;
-    return sc;
   }
   else {
     sc = rm->remoteLoadFile(fileName, fileHandle);
-    return sc;
   }
+  fileHandle.rm = rm;
+  fileHandle.bm = bm;
+  return sc;
 }
 
 StatusCode DS_RemoteManager::remoteLoadFile(const char *fileName,
                                             DS_FileH &fileHandle)
 {
+  string file_header;
+  getRemoteHeaderFile(fileName, file_header);
+  fileHandle.isRemote = true;
+  fileHandle.hdr = strdup(file_header.c_str());
+  return DS_SUCCESS;
+}
 
+StatusCode DS_RemoteManager::getRemoteHeaderFile(const char *fileName,
+                                                 string &header_content)
+{
+  char *recv_msg = new char[DS_CHAR_BUF_SIZE];
+  memset(recv_msg, 0, DS_CHAR_BUF_SIZE);
+  string proto_msg("HeyHo!");
+  //makeProtocolMsg(DS_PROTO_LOAD_FILE, fileName, proto_msg);
+  sendrecvFrom(DS_NAME_SERVER, proto_msg, recv_msg);
+  //parse_msg(recv_msg, parse_obj);
+  //header_content = parse_obj->value;
+  header_content = recv_msg;
+  return DS_SUCCESS;
+}
+
+StatusCode DS_RemoteManager::sendrecvFrom(int server_code, string send_msg,
+                                                           string &recv_msg)
+{
+  boost::asio::ip::tcp::socket sock;
+  string server_addr = servers[server_code];
+  rawSendRecv(server_addr, send_msg, recv_msg);
+  return DS_SUCCESS;
+}
+
+StatusCode DS_RemoteManager::rawSendRecv(string server_addr,
+                                         string send_msg,
+                                         string &recv_msg)
+{
+  establishConn(server_addr, sock);
+  send_to(sock, send_msg);
+  recv_from(sock, recv_msg);
+  close_connection(sock);
+  return DS_SUCCESS;
+}
+
+StatusCode DS_RemoteManager::establishConn(string server_addr,
+                                           boost::asio::ip::tcp::socket &sock)
+{
+  boost::asio::io_service io_service;
+  tcp::resolver resolver(io_service);
+  tcp::resolver::query query(server_addr, "daytime");
+  tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+  tcp::socket socket(io_service);
+  boost::asio::connect(socket, endpoint_iterator);
+  sock = socket;
+  return DS_SUCCESS;
+}
+
+StatusCode DS_RemoteManager::sendTo(boost::asio::ip::tcp::socket &sock,
+                                    string send_msg)
+{
+  boost::system::error_code ignored_error;
+  boost::asio::write(socket, boost::asio::buffer(send_msg), ignored_error);
+  return DS_SUCCESS;
+}
+
+StatusCode DS_RemoteManager::recvFrom(boost::asio::ip::tcp::socket &sock,
+                                      string &recv_msg)
+{
+  boost::array<char, 128> buf;
+  boost::system::error_code error;
+
+  size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+  if (error == boost::asio::error::eof)
+    break; // Connection closed cleanly by peer.
+  else if (error)
+    throw boost::system::system_error(error); // Some other error.
+
+  recv_msg.assign(buf.data());
+  return DS_SUCCESS;
 }
